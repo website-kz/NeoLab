ad0aa4735a586c21d22ad657f860566eb94a40b6
site <manshh01o@gmail.com>
2025-07-24 20:33:21 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  win.loadFile('public/index.html');
+}
+
+app.whenReady().then(createWindow);
