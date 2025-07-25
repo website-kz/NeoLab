10ba8070b980356a1a40496a4fab543446e0e832
site <manshh01o@gmail.com>
2025-07-24 20:44:12 -0400

    Delete src/runner/runCSharp.jsx

--- src/runner/runCSharp.jsx
+++
@@ -1,9 +0,0 @@
-import { exec } from 'child_process';
-import fs from 'fs';
-
-export const runCSharp = (code) => new Promise((resolve) => {
-  fs.writeFileSync('Program.cs', code);
-  exec('dotnet run Program.cs', (err, stdout, stderr) => {
-    resolve(stderr || stdout);
-  });
-});
